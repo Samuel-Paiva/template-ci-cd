be1d6936881ee169c0a0aebbbce22889240342e7
Samuel-Paiva <106559067+Samuel-Paiva@users.noreply.github.com>
2025-02-18 22:26:35 +0000

    chore: fix  app.py

--- app/app.py
+++ app/app.py
@@ -5,9 +5,3 @@ app = Flask(__name__)
 @app.route("/")
 def pagina_inicial():
     return "Continuous Integration and Continuous Delivery"
-----
-
-16. dentro do diretório /app crie o arquivo requirements.txt
-----
-flask == 3.1.0
-gunicorn == 23.0.0
